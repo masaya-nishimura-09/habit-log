d9f3d870791c31aa4c0f1e12cd1ec1e5240f8414
Masaya <masaya.nishimura0927@gmail.com>
2025-11-30 21:41:11 +0900

    remove card component from new negative notes page

--- components/containers/negative-notes/new/new-negative-notes.tsx
+++ components/containers/negative-notes/new/new-negative-notes.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { NegativeNotesFormData } from "@/types/negative-notes";
 import InputDesiredState from "./input-fields/input-desired-state";
 import InputEmotion from "./input-fields/input-emotion";
