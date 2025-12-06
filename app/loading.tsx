c397fac27da76ceb63a4f9a60816e705357e4ae3
Masaya <masaya.nishimura0927@gmail.com>
2025-12-06 10:47:41 +0900

    fix loading page design

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,10 +1,23 @@
+import { IconNotebook } from "@tabler/icons-react";
+import {
+  Card,
+  CardContent,
+} from "@/components/ui/card";
 import { Spinner } from "@/components/ui/spinner";
 
 export default function Loading() {
   return (
-    <div className="mt-50 flex w-full text-secondary items-center justify-center gap-4">
-      <Spinner className="size-8" />
-      <span className="text-xl">読み込み中...</span>
+    <div className="min-h-screen flex justify-center items-center">
+      <Card className="w-sm md:w-md">
+        <div className="mb-2 flex items-center justify-center gap-2 text-primary">
+          <IconNotebook className="size-10 bg-red" />
+          <span className="font-semibold text-2xl">Habit Log</span>
+        </div>
+        <CardContent className="p-5 flex flex-col w-full text-primary items-center justify-center gap-4">
+          <Spinner className="size-24"/>
+          <span className="text-xl">Loading...</span>
+        </CardContent>
+      </Card>
     </div>
   );
 }
